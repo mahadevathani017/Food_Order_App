a49e0d00068f3ac75a5e1ecf0cbd096b4c8606c6
Mahadev Chidanand Athani <athanimahadev348@gmail.com>
2024-08-16 18:22:24 +0530

    add api

--- src/components/Body.js
+++ src/components/Body.js
@@ -17,24 +17,23 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9578269&lng=77.5981177&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9578269&lng=77.5981177&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
 
     const json = await data.json();
 
     
     setlistOfRestaurants(
-      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle.restaurants
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
     setFilteredRestaurant(
-    json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle.restaurants);
+    json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
   };
 
-  if (listOfRestaurants.length == 0) {
-    return <Shimmer />;
-  }
-
-  return (
+  return listOfRestaurants.length == 0 ?
+  (
+     <Shimmer />
+  ):(
     <div className="body">
       <div className="filter">
         <div className="search">
